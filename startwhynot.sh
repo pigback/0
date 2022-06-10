567630d818c15f301652004b6c45156de91b73e8
valetzx <45054400+valetzx@users.noreply.github.com>
2022-06-10 10:48:17 +0000

    add ls tmp floder

--- startwhynot.sh
+++ startwhynot.sh
@@ -1,7 +1,7 @@
 sudo zerotier-one -d
-sudo mkdir /tmp/onedrive
+#sudo mkdir /tmp/onedrive 'useless'
 sudo rclone mount e5: /tmp/onedrive --allow-non-empty --daemon --vfs-cache-mode writes --allow-other && df -hl
-
+sudo ls /tmp/onedrive/ && df -hl
 sudo docker restart xfce
 #sudo bt start
 sudo nethogs
