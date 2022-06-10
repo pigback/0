3947162f494cb28602ce771ac1c8a8e0012a283a
valetzx <45054400+valetzx@users.noreply.github.com>
2022-06-10 10:42:34 +0000

    add ls mount

--- startwhynot.sh
+++ startwhynot.sh
@@ -1,6 +1,7 @@
 sudo zerotier-one -d
 sudo mkdir /tmp/onedrive
-sudo rclone mount e5: /tmp/onedrive --allow-non-empty --daemon --vfs-cache-mode writes --allow-other
+sudo rclone mount e5: /tmp/onedrive --allow-non-empty --daemon --vfs-cache-mode writes --allow-other && df -hl
+
 sudo docker restart xfce
 #sudo bt start
 sudo nethogs
