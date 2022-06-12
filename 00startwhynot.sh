eb2b37a8f6c13f590db955f08a6bc1c828f99222
valetzx <45054400+valetzx@users.noreply.github.com>
2022-06-12 13:38:05 +0000

    change

--- 00startwhynot.sh
+++ 00startwhynot.sh
@@ -1,8 +1,9 @@
 sudo zerotier-one -d
-sudo mkdir /tmp/onedrive
+#sudo mkdir /tmp/onedrive
 sudo rclone mount e5: /tmp/onedrive --allow-non-empty --daemon --vfs-cache-mode writes --allow-other && df -hl
 sudo ls /tmp/onedrive/
-sudo docker restart xfce
-#sudo bt restart
+sudo docker restart xubuntu
+sudo docker restart ubuntu
+sudo bt restart
 #sudo vncserver :1  
 sudo nethogs
--- tmpwrite.py
+++ tmpwrite.py
@@ -1,4 +1,5 @@
-2022-06-12+13:19:31+Codespaces-tmpwrite.py-spacest[Codespaces]-VisualtudioCode
-2022-06-12+21:19:31+float96-tmpwrite.py-spacest[float96]-VisualtudioCode
-2022-06-12+21:19:31+float96-tmpwrite.py-spacest[float96]-VisualtudioCode
-da69d4968+2516583408-tmpwrite.py-spacest[da69d4968]-VisualtudioCode 
+2022-06-12+13:29:25+Codespaces-tmpwrite.py-spacest[Codespaces]-VisualtudioCode
+2022-06-12+21:29:25+c5567fc44
+2022-06-12+21:29:25+c5567fc44
+2022-06-12+21:29:25+c5567fc44
+ad0d6166c+25165834  +25165834 
