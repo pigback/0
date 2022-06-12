8e33b6abff41e24b312121d9e920c545cd50bb2f
valetzx <45054400+valetzx@users.noreply.github.com>
2022-06-12 02:40:41 +0000

    re

--- 03rcloneinstall.sh
+++ 03rcloneinstall.sh
@@ -3,8 +3,8 @@ apt-get install fuse
 #sudo curl https://rclone.org/install.sh | sudo bash
 #这个文件是用来帮助我自动安装 macos-docker 的
 #启动命令为cd macos && docker-compose up -d
-#sudo mkdir -p ~/.config/rclone/
-#sudo unzip -P ${{ secrets.PASSWD }} rclone.zip -d ~/.config/rclone/
+#sudo mkdir -p /root/.config/rclone/
+#sudo unzip -P ${{ secrets.PASSWD }} rclone.zip -d /root/.config/rclone/
 sudo rclone lsf e5:/codespace/macos-docker/ > "rclone5dir.log"
 sudo rclone copy e5:/codespace/macos-docker/firmware/ ./macos/firmware/ -P
 sudo rclone copy e5:/codespace/macos-docker/docker-compose.yml ./macos -P
