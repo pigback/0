4e72a6ab8fab5a153f0677c6e6d7ac774bd5b081
valetzx <45054400+valetzx@users.noreply.github.com>
2022-06-13 20:55:37 +0800

    Update and rename 33refre.sh to 33xdorefre.sh

--- 33xdorefre.sh
+++ 33xdorefre.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
+#整个这个列表的快捷键都要自己设置，可以设置成别的，对应代码按键也要改！
 pid_list=`xdotool search --name "space"`
 xdotool windowfocus $pid_list
 xdotool search --name "space" key "shift+r"
-#refresh explorer
+#refresh explorer 
 xdotool search --name "space" key "shift+r"
+#需要自己设置 vscode web 快捷键 refresh explorer 
 sleep 2
 xdotool search --name "space" key "shift+s"
 #stage all changes
@@ -14,6 +16,7 @@ sleep 10
 xdotool search --name "space" type change
 sleep 10
 xdotool search --name "space" key "Return"
+#commit 需要输入内容
 sleep 10
 xdotool search --name "space" key "shift+n"
 #git sync
