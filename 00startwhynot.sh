bd0096b0ed92b181c3d0040500929e381ab7bc99
valetzx <45054400+valetzx@users.noreply.github.com>
2022-06-14 22:27:13 +0800

    Update 00startwhynot.sh

--- 00startwhynot.sh
+++ 00startwhynot.sh
@@ -2,7 +2,7 @@ sudo zerotier-one -d
 sleep 2
 sudo rm -rf /tmp/onedrive
 sudo mkdir /tmp/onedrive
-sudo rclone mount e5: /tmp/onedrive --allow-non-empty --daemon --vfs-cache-mode writes --allow-other && df -hl
+sudo rclone mount e5: /tmp/onedrive --allow-non-empty --daemon --vfs-cache-mode writes --allow-other --cache-dir /tmp && df -hl
 sudo ls /tmp/onedrive/
 sudo docker restart xubuntu
 sudo docker restart ubuntu
