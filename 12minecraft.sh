2a5984fbce94aa549f0367c028cda6f45d23ac9d
valetzx <45054400+valetzx@users.noreply.github.com>
2022-06-23 12:48:18 +0000

    add minecraft

---
+++ 12minecraft.sh
@@ -0,0 +1,26 @@
+cd /tmp/onedrive/mcserver/
+java -Xmx1024M -Xms1024M -jar server.jar nogui
+
+#hello guys, today ill take three dupe bug on minecraft one point onenine 
+#the first is Shulker Box dupe its very easy to use and is the most popular one
+#we put some items which we want to dupe in to Shulker Box and then click dupe all
+#the dupe mod will help us broken box and pull items to our inventory
+#and the box have items keeped 
+
+#then the second is Chest Boat dupe，
+#you will need a friend or another account to complete this
+#let your friend sit on boat and put items you wanna dupe in chest
+#and then your friend click ready! bottom
+#at the same time you click dupe all bottom
+#the dupe mod will dupe items for you
+
+#the third one is Allay dupe 
+#the new mob can help us dupe item like dolphin
+#drop the item you wanna dupe beside the nether portal
+#then give the mark item to Allay and open trapdoor
+#Allay will run to the mark item and pick it up
+#and then allay port to nether and at the same time
+#the picked item will drop to note block mybe
+#whatever you will get two item you wanna dupe
+#im chinese and my english is not well 
+#thank you all have fun!
