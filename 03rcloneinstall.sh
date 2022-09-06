d9f3a9e8a28dcd1f9eb00def2e60072acd8d6e42
pigback <98007722+pigback@users.noreply.github.com>
2022-09-06 01:15:36 +0000

    update

--- 03rcloneinstall.sh
+++ 03rcloneinstall.sh
@@ -4,7 +4,7 @@ apt-get install fuse
 #这个文件是用来帮助我自动安装 macos-docker 的
 #启动命令为cd macos && docker-compose up -d
 #sudo mkdir -p /root/.config/rclone/
-#sudo unzip -P ${{ secrets.PASSWD }} rclone.zip -d /root/.config/rclone/
+#sudo unzip -P ${{PASSWD }} rclone.zip -d /root/.config/rclone/
 sudo rclone lsf e5:/codespace/macos-docker/ > "rclone5dir.log"
 sudo rclone copy e5:/codespace/macos-docker/firmware/ ./macos/firmware/ -P
 sudo rclone copy e5:/codespace/macos-docker/docker-compose.yml ./macos -P
